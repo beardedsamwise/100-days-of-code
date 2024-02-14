9b39260472c154b0a1469e9789ad5f1172e4573a
Sam Bentley <sam.bentley@withriley.com>
2024-02-14 16:15:31 +1100

    wip!

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -1,7 +1,7 @@
 # TODO: allow hidden operation of "off" to exit program
-# TODO: Function: Process coins, if enough money presented store the money, offer change, if not return false
 
 from art import CUP
+import os
 
 def print_art():
     """Prints coffee cup"""
@@ -13,7 +13,7 @@ def prompt_user():
 
 def print_report(resources):
     """Prints a report of the current resources in the coffee machine."""
-    print(f"Water:  {resources["water"]}ml")
+    print(f"\nWater:  {resources["water"]}ml")
     print(f"Milk:   {resources["milk"]}ml")
     print(f"Coffee: {resources["coffee"]}g")
     print(f"Money:  ${resources["money"]}")
@@ -31,12 +31,67 @@ def check_resources(res_current, res_request):
         return True
     return False
 
+def process_coins(req_amount):
+    value = 0.0
+    change = 0.0
+    coins_provided = {
+    "quarters" : 0,
+    "dimes" : 0,
+    "nickles" : 0,
+    "pennies" : 0
+    }
+
+    coins_provided["quarters"] = input("Insert the number of Quarters you have: ")
+    coins_provided["dimes"] = input("Insert the number of Dimes you have: ")
+    coins_provided["nickles"] = input("Insert the number of Nickles you have: ")
+    coins_provided["pennies"] = input("Insert the number of Pennies you have: ")
+    value += coins_provided["quarters"] * COIN_VALUES["quarter"]
+    value += coins_provided["dimes"] * COIN_VALUES["dime"]
+    value += coins_provided["nickles"] * COIN_VALUES["nickle"]
+    value += coins_provided["pennies"] * COIN_VALUES["pennie"]
+
+    if value < req_amount:
+        enough_money = False
+        return value, change, enough_money
+    else:
+        change = value - req_amount
+        enough_money = True
+        return value, change
+
+COIN_VALUES = {
+    "quarter" : 0.25,
+    "dime" : 0.10,
+    "nickle" : 0.05,
+    "pennie" : 0.01
+}
+
+PRICES = {
+    "espresso" : 2.0,
+    "latte" : 2.5,
+    "cappuccino" : 2.60
+}
+
 resources = {
     "water" : 300,
     "milk" : 50,
     "coffee" : 76,
     "money" : 2.5
 }
-print_art()
-prompt_user()
+
+machine_on = True
+
+while machine_on:
+    print_art()
+    user_input = prompt_user() 
+    if user_input in PRICES.keys():
+        print("You ordered a coffee")
+    elif user_input == "off":
+        machine_on = False
+    elif user_input == "report":
+        print_report(resources)
+    else:
+        print("You didn't select a valid option...")
+
+print("The coffee machine has been switched off. GOOD BYE!")
+
 print_report(resources)
